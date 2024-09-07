7fb486e62f9c8eba3d3af9ca72dd62370ec70cc0
UsoltsevI <usoltsev.ia@phystech.edu>
2024-09-07 11:25:18 +0300

    First Spring Boot version

---
+++ src/main/java/org/example/SpringOneApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringOneApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOneApp.class, args);
+    }
+}
